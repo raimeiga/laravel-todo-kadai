dc2eecddec9e76119d9d599114ee47b9784b4b9f
raimeiga <yusuke612@hotmail.com>
2023-11-03 01:44:02 +0900

    詳細（description）データをDBに保存できるようstoreとupdateアクション修正

--- kadai_013/laravel-todo-app/app/Http/Controllers/TodoController.php
+++ kadai_013/laravel-todo-app/app/Http/Controllers/TodoController.php
@@ -33,6 +33,7 @@ class TodoController extends Controller
 
         $todo = new Todo();
         $todo->content = $request->input('content');
+        $todo->description = $request->input('description');
         $todo->user_id = Auth::id();  //←現在ログイン中のユーザーが持つ目標をすべて取得
         $todo->goal_id = $goal->id;
         $todo->done = false;
@@ -62,6 +63,7 @@ class TodoController extends Controller
         ]);
 
         $todo->content = $request->input('content');
+        $todo->description = $request->input('description');
         $todo->user_id = Auth::id();
         $todo->goal_id = $goal->id;
         $todo->done = $request->boolean('done', $todo->done);
